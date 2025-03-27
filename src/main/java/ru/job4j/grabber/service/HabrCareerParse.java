920b11f9fc03ab9ba75bd2b0f5460eb54e1a0132
Aleksandr Prigodich <64789713+Olegsander48@users.noreply.github.com>
2025-03-27 09:07:01 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
@@ -17,38 +18,49 @@ public class HabrCareerParse implements Parse {
     private static final String PREFIX = "/vacancies?page=";
     private static final String SUFFIX = "&q=Java%20developer&type=all";
     private static final int VACANCY_PAGES = 5;
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     @Override
     public List<Post> fetch() {
         var result = new ArrayList<Post>();
-        try {
-            for (int pageNumber = 1; pageNumber <= VACANCY_PAGES; pageNumber++) {
-                String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-                var connection = Jsoup.connect(fullLink);
-                var document = connection.get();
-                var rows = document.select(".vacancy-card__inner");
-                rows.forEach(row -> {
-                    var titleElement = row.select(".vacancy-card__title").first();
-                    var linkElement = titleElement.child(0);
-                    var created = row.select(".vacancy-card__date").first();
-                    String vacancyName = titleElement.text();
-                    String link = String.format("%s%s", SOURCE_LINK,
-                            linkElement.attr("href"));
-                    var post = new Post();
-                    post.setTitle(vacancyName);
-                    post.setLink(link);
-                    post.setDescription(retrieveDescription(link));
-                    post.setTime(new HabrCareerDateTimeParser()
-                            .parse(created.child(0).attr("datetime"))
-                            .atZone(ZoneId.systemDefault())
-                            .toEpochSecond());
-                    result.add(post);
-                });
-            }
-        } catch (IOException e) {
-            LOG.error("When load page", e);
+        for (int pageNumber = 1; pageNumber <= VACANCY_PAGES; pageNumber++) {
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            result.addAll(list(fullLink));
         }
         return result;
+    }
+
+    @Override
+    public List<Post> list(String fullLink) {
+        List<Post> posts = new ArrayList<>();
+        var connection = Jsoup.connect(fullLink);
+        try {
+            Document document = connection.get();
+            var rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                var titleElement = row.select(".vacancy-card__title").first();
+                var linkElement = titleElement.child(0);
+                var created = row.select(".vacancy-card__date").first();
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                var post = new Post(
+                        vacancyName,
+                        link,
+                        retrieveDescription(link),
+                        dateTimeParser.parse(created.child(0)
+                                        .attr("datetime"))
+                                .atZone(ZoneId.systemDefault())
+                                .toEpochSecond());
+                posts.add(post);
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return posts;
     }
 
     private String retrieveDescription(String link) {
@@ -71,7 +83,7 @@ public class HabrCareerParse implements Parse {
     }
 
     public static void main(String[] args) {
-        HabrCareerParse parse = new HabrCareerParse();
+        HabrCareerParse parse = new HabrCareerParse(new HabrCareerDateTimeParser());
         parse.fetch().forEach(System.out::println);
     }
 }
